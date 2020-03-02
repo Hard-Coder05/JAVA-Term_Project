b82284bf6c934b58233da126487213a411b8cfc5
Hard-Coder05 <smishra1605@gmail.com>
2020-03-03 01:00:43 +0530

    Finally Completed now

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -30,10 +30,8 @@ public class MainActivity extends AppCompatActivity {
     private double val1=Double.NaN;
     private char val2 ='+';
     private double res =0;
-
-
-
-
+    private int i=1;
+    private int p=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,9 +43,18 @@ public class MainActivity extends AppCompatActivity {
         zero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(p!=1){
+                    info.setText("");
+                    chodu.setText("");
+                    val1=Double.NaN;
+                    val2 ='+';
+                    res =0;
+                    p=1;
+                }
                 info.setText(info.getText().toString()+"0");
                 chodu.setText(chodu.getText().toString()+"0");
                 val1 = Double.parseDouble(chodu.getText().toString());
+                i=1;
             }
         });
         clr.setOnClickListener(new View.OnClickListener() {
@@ -58,142 +65,257 @@ public class MainActivity extends AppCompatActivity {
                 res=0;
                 val1=Double.NaN;
                 val2='+';
+                i=1;
             }
         });
         equals.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 if(!Double.isNaN(val1)){
-
                     compute();
-
                     info.setText(""+res);
                 chodu.setText(""+res);
                 //res=0;
                 val1=Double.NaN;
-                val2='+';}
+                    i=1;
+                val2='+';
+                p=0;}
                 else{
-
                     info.setText(""+res);
                     chodu.setText(""+res);
+                    p=0;
                 }
             }
         });
         one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(p!=1){
+                    info.setText("");
+                    chodu.setText("");
+                    val1=Double.NaN;
+                    val2 ='+';
+                    res =0;
+                    p=1;
+                }
                 info.setText(info.getText().toString()+"1");
                 chodu.setText(chodu.getText().toString()+"1");
                 val1 = Double.parseDouble(chodu.getText().toString());
+                i=1;
             }
         });
         two.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(p!=1){
+                    info.setText("");
+                    chodu.setText("");
+                    val1=Double.NaN;
+                    val2 ='+';
+                    res =0;
+                    p=1;
+                }
                 info.setText(info.getText().toString()+"2");
                 chodu.setText(chodu.getText().toString()+"2");
                 val1 = Double.parseDouble(chodu.getText().toString());
+                i=1;
             }
         });
         three.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(p!=1){
+                    info.setText("");
+                    chodu.setText("");
+                    val1=Double.NaN;
+                    val2 ='+';
+                    res =0;
+                    p=1;
+                }
                 info.setText(info.getText().toString()+"3");
                 chodu.setText(chodu.getText().toString()+"3");
                 val1 = Double.parseDouble(chodu.getText().toString());
+                i=1;
             }
         });
         four.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(p!=1){
+                    info.setText("");
+                    chodu.setText("");
+                    val1=Double.NaN;
+                    val2 ='+';
+                    res =0;
+                    p=1;
+                }
                 info.setText(info.getText().toString()+"4");
                 chodu.setText(chodu.getText().toString()+"4");
                 val1 = Double.parseDouble(chodu.getText().toString());
+                i=1;
             }
         });
         five.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(p!=1){
+                    info.setText("");
+                    chodu.setText("");
+                    val1=Double.NaN;
+                    val2 ='+';
+                    res =0;
+                    p=1;
+                }
                 info.setText(info.getText().toString()+"5");
                 chodu.setText(chodu.getText().toString()+"5");
                 val1 = Double.parseDouble(chodu.getText().toString());
+                i=1;
             }
         });
         six.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(p!=1){
+                    info.setText("");
+                    chodu.setText("");
+                    val1=Double.NaN;
+                    val2 ='+';
+                    res =0;
+                    p=1;
+                }
                 info.setText(info.getText().toString()+"6");
                 chodu.setText(chodu.getText().toString()+"6");
                 val1 = Double.parseDouble(chodu.getText().toString());
+                i=1;
             }
         });
         seven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(p!=1){
+                    info.setText("");
+                    chodu.setText("");
+                    val1=Double.NaN;
+                    val2 ='+';
+                    res =0;
+                    p=1;
+                }
                 info.setText(info.getText().toString()+"7");
                 chodu.setText(chodu.getText().toString()+"7");
                 val1 = Double.parseDouble(chodu.getText().toString());
+                i=1;
             }
         });
         eight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(p!=1){
+                    info.setText("");
+                    chodu.setText("");
+                    val1=Double.NaN;
+                    val2 ='+';
+                    res =0;
+                    p=1;
+                }
                 info.setText(info.getText().toString()+"8");
                 chodu.setText(chodu.getText().toString()+"8");
                 val1 = Double.parseDouble(chodu.getText().toString());
+                i=1;
             }
         });
         nine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(p!=1){
+                    info.setText("");
+                    chodu.setText("");
+                    val1=Double.NaN;
+                    val2 ='+';
+                    res =0;
+                    p=1;
+                }
                 info.setText(info.getText().toString()+"9");
                 chodu.setText(chodu.getText().toString()+"9");
                 val1 = Double.parseDouble(chodu.getText().toString());
+                i=1;
             }
         });
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                info.setText(info.getText().toString()+"+");
-                chodu.setText("");
-
-                compute();
-
-                val2='+';
-
+                if(p!=1){
+                    info.setText("");
+                    chodu.setText("");
+                    val1=Double.NaN;
+                    val2 ='+';
+                    res =0;
+                    p=1;
+                }
+                if(i!=0) {
+                    info.setText(info.getText().toString() + "+");
+                    chodu.setText("");
+                    compute();
+                    val2 = '+';
+                    i=0;
+                }
             }
         });
         sub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(p!=1){
+                    info.setText("");
+                    chodu.setText("");
+                    val1=Double.NaN;
+                    val2 ='+';
+                    res =0;
+                    p=1;
+                }
+                if(i!=0){
                 info.setText(info.getText().toString()+"-");
                 chodu.setText("");
                 compute();
                 val2='-';
-            }
+                    i=0;
+            }}
         });
         mul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(p!=1){
+                    info.setText("");
+                    chodu.setText("");
+                    val1=Double.NaN;
+                    val2 ='+';
+                    res =0;
+                    p=1;
+                }
+                if(i!=0){
                 info.setText(info.getText().toString()+"*");
                 chodu.setText("");
                 compute();
                 val2='*';
-            }
+                    i=0;
+            }}
         });
         div.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(p!=1){
+                    info.setText("");
+                    chodu.setText("");
+                    val1=Double.NaN;
+                    val2 ='+';
+                    res =0;
+                    p=1;
+                }
+                if(i!=0){
                 info.setText(info.getText().toString()+"/");
                 chodu.setText("");
                 compute();
                 val2='/';
-            }
+                    i=0;
+            }}
         });
     }
 
